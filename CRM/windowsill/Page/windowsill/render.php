11b815625ac6d824376348956b4f6fc86b977acf
kewljuice <kewljuice@hotmail.com>
2015-11-04 17:46:45 +0100

    Service
    Created a views render page as service to fetch & render settingsname

---
+++ CRM/windowsill/Page/windowsill/render.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_windowsill_Page_windowsill_render extends CRM_Core_Page {
+  function run() {
+		
+		// get html from view by setting_name
+		$setting = $_REQUEST['name'];
+		
+    // get settings from civicrm
+		$settings = CRM_Core_BAO_Setting::getItem('windowsill', 'settings');
+		$decode = json_decode(utf8_decode($settings), true);
+		
+		// variables
+		$error = TRUE;
+		$content = "";
+		$view = array();
+				
+		// loop settings		
+		foreach ($decode as &$value) {
+    	// get view data if setting exists
+			if($setting  == $value['name']) {
+				$error = FALSE;
+				$view = explode(':',$value['view']);
+				break;
+			}
+		}
+		
+		// check if setting exists & set page variables
+		if(!$error) { 
+			$title = $setting;    
+			$content .= views_embed_view($view[0], $view[1]) . "<br>";
+		} else {
+			$title = "Configuration error";    
+			$content .= "Check your windowsill settings.";
+		}
+		
+		// assign page title
+    CRM_Utils_System::setTitle(ts('WindowSill Render'));
+		
+	  // assign a variable for use in a template
+    $this->assign('title', $title);
+		$this->assign('content', $content);
+	
+		// render
+    parent::run();
+  }
+}
---
+++ templates/CRM/windowsill/Page/windowsill/render.tpl
@@ -0,0 +1,5 @@
+{* Display title *}
+<h3>{$title}</h3>
+{* Display content *}
+<p>{$content}</p>
+
