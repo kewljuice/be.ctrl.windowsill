b1707c116d190e2f406fc93da3c55a95fef02a6f
kewljuice <kewljuice@hotmail.com>
2015-11-05 21:03:40 +0100

    Token
    Fix rendering issues

--- php/hooks.php
+++ php/hooks.php
@@ -1,11 +1,7 @@
 <?php
 
 /**
- * CiviCRM hooks
- */
-
-/*
- * implementation of CiviCRM navigationMenu hook
+ * implementation of CiviCRM navigationMenu hook.
  */
 function windowsill_civicrm_navigationMenu(&$params) {
 	//  Get the maximum key of $params.
@@ -55,8 +51,8 @@ function windowsill_civicrm_navigationMenu(&$params) {
 	$params[$AdministerKey]['child'][$parentKey]['child'][$nextKey]  = $child;
 }
 
-/*
- * implementation of CiviCRM tabs hook
+/**
+ * implementation of CiviCRM tabs hook.
  */
 function windowsill_civicrm_tabs( &$tabs, $contactID ) {
 	// Get settings from civicrm
@@ -74,8 +70,8 @@ function windowsill_civicrm_tabs( &$tabs, $contactID ) {
 	}
 }
 
-/*
- * implementation of CiviCRM tokens hook
+/**
+ * implementation of CiviCRM tokens hook.
  */
 function windowsill_civicrm_tokens(&$tokens) {
 	// Register tokens
@@ -94,39 +90,30 @@ function windowsill_civicrm_tokens(&$tokens) {
 	}
 }
 
-/*
- * implementation of CiviCRM tokenValues hook
+/**
+ * implementation of CiviCRM tokenValues hook.
  */
 function windowsill_civicrm_tokenValues(&$values,&$contactIDs,$jobID,$tokens=array(),$context=null) {
-	// Log
-	watchdog('windowsill', 'civicrm_tokenValues hit');
-	
 	// Get settings from civicrm
 	$settings = CRM_Core_BAO_Setting::getItem('windowsill', 'settings');
-	$decode 	= json_decode(utf8_decode($settings), true);
-	
+	$decode = json_decode(utf8_decode($settings), true);
 	// Loop settings	
 	foreach ($decode as &$value) {
 		// Check if tab is configured
 		if($value['token']) {
-			$name	= str_replace(' ','_',$value['name']);
-			$view	= explode(':',$value['view']);
+			$name = str_replace(' ','_',$value['name']);
+			$view = explode(':',$value['view']);
 			// Rendered
-			$rendered = "hello";
+			$rendered = views_embed_view($view[0], $view[1]) ;
+			/*
 			// absolute paths are set to url paths (Anchor tags)
 			//$rendered['windowsill:'.$name] = preg_replace('/<\s*a\s(.*?)href="\/(.*?)"(.*?)>/i', '<a $1href="'.$base_url.$base_path.'$2"$3>', $rendered['windowsill:'.$name]);
 			//$rendered['windowsill:'.$name] = preg_replace('/<\s*img\s(.*?)src="\/(.*?)"(.*?)>/i', '<img $1src="'.$base_url.$base_path.'$2"$3>', $rendered['windowsill:'.$name]);
-	
-			// Log
-			watchdog('windowsill', "civicrm_tokenValues hit $name".print_r($rendered, true));
-		
+			*/
 			// Set token
-			foreach ($contactIDs as $contactID) { $values[$contactID]['windowsill'.$name] = $rendered; }
+			foreach ($contactIDs as $contactID) { $values[$contactID]['windowsill.'.$name] = $rendered; }
 		}
-	
 	}
 }
 			
-
-
 ?>
